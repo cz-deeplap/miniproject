249f962eba1a64475364328098edb02265f829a4
czalo <iceeiceee4@gmail.com>
2024-09-25 00:53:12 +0700

    draft

--- draft.php
+++ draft.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>จัดการ padding width height ตามขนาดจอ</h1>
+    <h1> - จัดการ padding width height ตามขนาดจอ</h1>
+    <h1> - ทำการบ้านเรื่องอัพขึ้นเซริฟ</h1>
 </body>
 </html>
